61ff3da67b73babf37cdf40d720d1b42349ff997
Simple <1853967891@qq.com>
2025-05-28 22:57:07 +0800

    编写四种方式调用大模型demo。sdk、api、spring ai、LangChain4J

---
+++ src/main/java/com/simple/aiagent/demo/invoke/LangChainAiInvoke.java
@@ -0,0 +1,19 @@
+package com.simple.aiagent.demo.invoke;
+
+import dev.langchain4j.community.model.dashscope.QwenChatModel;
+import dev.langchain4j.model.chat.ChatLanguageModel;
+
+/**
+ * LangChain4J框架调用通义大模型
+ */
+public class LangChainAiInvoke {
+
+    public static void main(String[] args) {
+        ChatLanguageModel qwenModel = QwenChatModel.builder()
+                .apiKey(TestApiKey.API_KEY)
+                .modelName("qwen-plus")
+                .build();
+        String answer = qwenModel.chat("你好，我是simple");
+        System.out.println(answer);
+    }
+}
